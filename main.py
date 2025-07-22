304db5472359ba7482c13cf6ccd96167ec8253f9
jaakkv <jaakko.vaaramaki@gmail.com>
2025-07-22 23:05:34 +0300

    C: updated main.py to calculate words in book

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+#def get_book_text(filepath):
+#    with open(filepath) as f:
+#        content = f.read()
+#        print(content)
+
+def number_of_words(filepath):
+    content = []
+    with open(filepath) as f:
+        content = f.read().split()
+        print(f"{len(content)} words found in the document")
+
+def main():
+    number_of_words('books/frankenstein.txt')
+if __name__ == '__main__':
+    main()
