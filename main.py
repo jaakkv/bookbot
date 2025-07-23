ecfbebe90090ab356e6b55f74df2643b2b10ecff
jaakkv <jaakko.vaaramaki@gmail.com>
2025-07-23 22:41:23 +0300

    E: Updated main.py and stats.py to count characters

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from stats import number_of_words
+from stats import *
 
 def main():
     print(number_of_words('books/frankenstein.txt'))
+    print(number_of_characters('books/frankenstein.txt'))
 if __name__ == '__main__':
     main()
--- stats.py
+++ stats.py
@@ -1,5 +1,21 @@
 def number_of_words(filepath):
     content = []
+
     with open(filepath) as f:
         content = f.read().split()
         return f"{len(content)} words found in the document"
+
+def number_of_characters(filepath):
+    content = ""
+    char_dict = {}
+
+    with open(filepath) as f:
+        content = f.read()
+
+    for char in content:
+        if char.lower() in char_dict:
+            char_dict[char.lower()] += 1
+        else:
+            char_dict[char.lower()] = 1
+
+    return char_dict
